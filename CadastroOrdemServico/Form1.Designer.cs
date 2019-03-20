5fcc2eb27c40674935aad3b3d9e2ee5b48ce494d
tortelle <42223675+tortelle@users.noreply.github.com>
2019-03-20 10:58:23 -0300

    Versão 0.1

---
+++ CadastroOrdemServico/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace CadastroOrdemServico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menu = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.data2 = new System.Windows.Forms.MaskedTextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.data = new System.Windows.Forms.MaskedTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblHora = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.AutoSize = true;
+            this.menu.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.menu.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menu.ForeColor = System.Drawing.Color.Cyan;
+            this.menu.Location = new System.Drawing.Point(87, 14);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(260, 26);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "Cadastro Ordem de Serviço";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Data do Pedido: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Àrea Solicitada: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(328, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = " -Departamento";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Executado por: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Data finalizado o serviço: ";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(113, 63);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(209, 20);
+            this.txt1.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(109, 216);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(261, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // data2
+            // 
+            this.data2.Location = new System.Drawing.Point(160, 251);
+            this.data2.Mask = "00/00/0000";
+            this.data2.Name = "data2";
+            this.data2.Size = new System.Drawing.Size(67, 20);
+            this.data2.TabIndex = 12;
+            this.data2.ValidatingType = typeof(System.DateTime);
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(213, 283);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(207, 20);
+            this.txt4.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 285);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(198, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Assinatura do responsavel da área: ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(86, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 60);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "GERAR CADASTRO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // data
+            // 
+            this.data.Location = new System.Drawing.Point(111, 99);
+            this.data.Mask = "00/00/0000";
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(67, 20);
+            this.data.TabIndex = 16;
+            this.data.ValidatingType = typeof(System.DateTime);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.BackColor = System.Drawing.Color.White;
+            this.lblHora.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.Location = new System.Drawing.Point(322, 417);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(0, 14);
+            this.lblHora.TabIndex = 17;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.AutoSize = true;
+            this.groupBox3.BackColor = System.Drawing.Color.White;
+            this.groupBox3.Controls.Add(this.comboBox3);
+            this.groupBox3.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(12, 136);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(155, 67);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tipo de serviço: ";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Elétrica",
+            "Estrutura",
+            "Equipamento",
+            "Móveis/Ativos"});
+            this.comboBox3.Location = new System.Drawing.Point(11, 21);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 23);
+            this.comboBox3.TabIndex = 19;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(215, 136);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 67);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status do Cadastro: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Realizado",
+            "Terminado",
+            "Em Espera"});
+            this.comboBox1.Location = new System.Drawing.Point(11, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 19;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(113, 382);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 25);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Ver Cadastros";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(201, 411);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(36, 25);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(432, 444);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.data2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cadastro Ordem de Serviço";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label menu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.MaskedTextBox data2;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox data;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CadastroOrdemServico/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CadastroOrdemServico
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+                private void Form1_Load(object sender, EventArgs e )
+        {
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            //Cria a pasta no diretorio X
+            DirectoryInfo CriarPasta = new DirectoryInfo(@"C:\\Banco De Dados\\Cadastro Ordem Servico");
+
+            //Se Caso a pasta existir Returna para o FORM , caso NÃO ele cria
+            if (CriarPasta.Exists)
+            {
+                return;
+
+            }
+            else
+            {
+                CriarPasta.Create();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter arquivo;
+            String caminho = "C:\\Banco De Dados\\Cadastro Ordem Servico\\Cadastro Servicos.txt";
+            arquivo = File.AppendText(caminho);
+            arquivo.WriteLine(" ");
+            arquivo.WriteLine(menu.Text);
+            arquivo.WriteLine(label3.Text + txt1.Text + label4.Text);
+            arquivo.WriteLine(label2.Text + data.Text);
+            arquivo.WriteLine(groupBox3.Text + comboBox3.Text);
+            arquivo.WriteLine(groupBox1.Text + comboBox1.Text);
+            arquivo.WriteLine(label5.Text + textBox3.Text);
+            arquivo.WriteLine(label6.Text + data2.Text);
+            arquivo.WriteLine(label7.Text + txt4.Text);                     
+            arquivo.WriteLine("Cadastro Realizado: " + lblHora.Text + Environment.NewLine);
+            arquivo.WriteLine("=================================================================");
+            arquivo.Close();
+
+            //limpa todos os controles do tipo TextBoxx
+            txt1.Text = string.Empty;
+            data.Text = string.Empty;
+            comboBox3.Text = String.Empty;
+            comboBox1.Text = String.Empty;
+            textBox3.Text = String.Empty;
+            data2.Text = String.Empty;
+            txt4.Text = String.Empty;
+
+            MessageBox.Show("Cadastrado de serviço realizado com sucesso", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+
+
+            //Cria um botão com dialogo para continuar , S/N
+            DialogResult dialogResult = MessageBox.Show("Deseja cadastrar outro serviço ?", "Pergunta", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            //Cria um IF para o botao acima , se caso SIM , ele returna para o FORM e o lIMPA os campos , caso NÃO ele o FECHA !
+            if (dialogResult == System.Windows.Forms.DialogResult.Yes)
+            {                
+                return;
+            }
+
+            //Caso NÃO ele o FECHA !
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string caminho = "C:\\Banco De Dados\\Cadastro Ordem Servico\\Cadastro Servicos.txt";
+            System.Diagnostics.Process.Start("NOTEPAD", caminho);
+        }
+    }
+}
+
